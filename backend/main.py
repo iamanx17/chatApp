2f8e906bf93751971580f7b93468e4c42ffc32e5
Aman Mishra <iamanx17@gmail.com>
2025-01-21 12:57:05 +0000

    adding cors

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.user import user_router
 from routers.chat import chat_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 app.include_router(user_router, prefix='/user', tags=['User'])
 app.include_router(chat_router, prefix='/ws', tags=['Chat'])
+
 
 @app.get('/')
 def welcome_chat_app():
